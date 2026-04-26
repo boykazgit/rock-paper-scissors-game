f7aba91494b864f1b89cc4d263ba6a4658812c7c
Boyka <boykaannor@gmail.com>
2026-04-26 10:35:03 +0000

    Create Function To Generate Computer Choice

---
+++ script.js
@@ -0,0 +1,15 @@
+// Get computer choice
+//functio to return computer choice
+function getComputerChoice() {
+// Generate random number and assign it to a choiceNumber
+let choiceNumber = Math.random();
+let computerChoice;
+// Return computerChoice based on choiceNumber
+if (choiceNumber < (1/3)) {
+  return computerChoice = 'rock';
+}else if (choiceNumber > 1/3 && choiceNumber < 2/3){
+  return computerChoice = 'paper';
+}else {
+  return computerChoice = 'scissors';
+}
+}
